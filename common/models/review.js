d54062fe5a317fff40072d85a78f0896ae021f2d
gunjpan <gunjanp@ca.ibm.com>
2016-10-25 12:04:26 -0400

    Create Remote Hook

--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', function(context, user, next) {
+    context.args.data.date = Date.now();
+    // set `publisherId` fk that we mentioned in model relations to current userId
+    context.args.data.publisherId = context.req.accessToken.userId;
+    next();
+  });
 };
